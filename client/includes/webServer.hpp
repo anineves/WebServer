8b70c6ab4ab5c1b64af7089c2ff9758ef16b5856
abenjii <alexfern@student.42porto.com>
2024-04-07 21:05:02 +0100

    7 abril

--- client/includes/webServer.hpp
+++ client/includes/webServer.hpp
@@ -18,11 +18,25 @@
 # include <stdlib.h>
 # include <sys/epoll.h>
 
+// ------- variables
 # define SERVER_PORT 8080
 # define BUFFER_SIZE 30200
 # define MAX_CLIENTS 20
 # define MAX_EVENTS 20
 # define MAXEPOLLSIZE SOMAXCONN
+
+// ------- colors
+# define RESET   "\033[0m"
+# define BLACK   "\033[30m"
+# define RED     "\033[31m"
+# define GREEN   "\033[32m"
+# define YELLOW  "\033[33m"
+# define BLUE    "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN    "\033[36m"
+# define WHITE   "\033[37m"
+# define BOLD    "\033[1m"
+# define BLINK    "\033[5m"
 
 
 #endif
