ae75c9b8f103f2348ac34f2c2f52ec1acaf73ddb
Ana Neves <anineves5@gmail.com>
2024-05-26 16:47:05 +0100

    404

--- client/src/Response.cpp
+++ client/src/Response.cpp
@@ -8,30 +8,30 @@ Response::Response(Server &server) : m_server(server)
 {
     m_server = server;
     _default_error["413"] = "./errorpages/erro413.html";
-     _default_error["400"] = "./errorpages/erro400.html";
-     _default_error["401"] = "./errorpages/erro401.html";
-     _default_error["403"] = "./errorpages/erro403.html";
-     _default_error["404"] = "./errorpages/erro404.html";
-     _default_error["405"] = "./errorpages/erro405.html";
-     _default_error["406"] = "./errorpages/erro406.html";
-     _default_error["407"] = "./errorpages/erro407.html";
-     _default_error["408"] = "./errorpages/erro408.html";
-     _default_error["409"] = "./errorpages/erro409.html";
-     _default_error["410"] = "./errorpages/erro410.html";
-     _default_error["411"] = "./errorpages/erro411.html";
-     _default_error["412"] = "./errorpages/erro412.html";
-     _default_error["413"] = "./errorpages/erro413.html";
-     _default_error["414"] = "./errorpages/erro414.html";
-     _default_error["500"] = "./errorpages/erro500.html";
-     _default_error["501"] = "./errorpages/erro501.html";
-     _default_error["502"] = "./errorpages/erro402.html";
-     _default_error["503"] = "./errorpages/erro503.html";
-     _default_error["504"] = "./errorpages/erro504.html";
-     _default_error["505"] = "./errorpages/erro505.html";
-     _default_error["506"] = "./errorpages/erro506.html";
-     _default_error["507"] = "./errorpages/erro507.html";
-     _default_error["510"] = "./errorpages/erro510.html";
-     _default_error["511"] = "./errorpages/erro511.html";
+    _default_error["400"] = "./errorpages/erro400.html";
+    _default_error["401"] = "./errorpages/erro401.html";
+    _default_error["403"] = "./errorpages/erro403.html";
+    _default_error["404"] = "./errorpages/erro404.html";
+    _default_error["405"] = "./errorpages/erro405.html";
+    _default_error["406"] = "./errorpages/erro406.html";
+    _default_error["407"] = "./errorpages/erro407.html";
+    _default_error["408"] = "./errorpages/erro408.html";
+    _default_error["409"] = "./errorpages/erro409.html";
+    _default_error["410"] = "./errorpages/erro410.html";
+    _default_error["411"] = "./errorpages/erro411.html";
+    _default_error["412"] = "./errorpages/erro412.html";
+    _default_error["413"] = "./errorpages/erro413.html";
+    _default_error["414"] = "./errorpages/erro414.html";
+    _default_error["500"] = "./errorpages/erro500.html";
+    _default_error["501"] = "./errorpages/erro501.html";
+    _default_error["502"] = "./errorpages/erro402.html";
+    _default_error["503"] = "./errorpages/erro503.html";
+    _default_error["504"] = "./errorpages/erro504.html";
+    _default_error["505"] = "./errorpages/erro505.html";
+    _default_error["506"] = "./errorpages/erro506.html";
+    _default_error["507"] = "./errorpages/erro507.html";
+    _default_error["510"] = "./errorpages/erro510.html";
+    _default_error["511"] = "./errorpages/erro511.html";
 }
 
 Response::~Response()
@@ -60,27 +60,27 @@ std::string Response::buildErrorResponse(int code)
         response += "Content-Type: " + getContentType(getDefaultError(codestr).c_str()) + "\r\n\r\n";
 
         response += fileContent;
-    
     }
-    else {
-    response = "HTTP/1.1 " + codestr + " " + responseStatus(code) + "\r\n";
-    response += "Content-Type: text/html\r\n\r\n";
-    std::string fileContent = "<!DOCTYPE html>\n"
-                              "<html lang=\"en\">\n"
-                              "<head>\n"
-                              "    <meta charset=\"UTF-8\">\n"
-                              "    <link rel=\"stylesheet\" href=\"error.css\">\n"
-                              "    <title>Oops! Page Not Found</title>\n"
-                              "</head>\n"
-                              "<body>\n"
-                              "    <header>\n"
-                              "        <h1>404</h1>\n"
-                              "        <p>Oops! Page not found </p>\n"
-                              "    </header>\n"
-                              "</body>\n"
-                              "</html>";
-    response += fileContent;
-}
+    else
+    {
+        response = "HTTP/1.1 " + codestr + " " + responseStatus(code) + "\r\n";
+        response += "Content-Type: text/html\r\n\r\n";
+        std::string fileContent = "<!DOCTYPE html>\n"
+                                  "<html lang=\"en\">\n"
+                                  "<head>\n"
+                                  "    <meta charset=\"UTF-8\">\n"
+                                  "    <link rel=\"stylesheet\" href=\"error.css\">\n"
+                                  "    <title>Oops! Page Not Found</title>\n"
+                                  "</head>\n"
+                                  "<body>\n"
+                                  "    <header>\n"
+                                  "        <h1>404</h1>\n"
+                                  "        <p>Oops! Page not found </p>\n"
+                                  "    </header>\n"
+                                  "</body>\n"
+                                  "</html>";
+        response += fileContent;
+    }
     return response;
 }
 
@@ -90,30 +90,49 @@ std::string Response::buildResponse(Request request, Location &location)
     if (request.getMethod() == "GET" || request.getMethod() == "POST" || request.getMethod() == "DELETE")
     {
         std::string filePath = obtainFilePath(request.getPath(), location);
-        std::ifstream file(filePath.c_str());
-        if (file)
+        if (!isValidPath(filePath))
         {
-            std::stringstream content;
-            content << file.rdbuf();
-            std::string fileContent = content.str();
-            std::stringstream length;
-            length << fileContent.size();
-            std::stringstream stringCode;
-            stringCode << request.getCode();
-            response = "HTTP/1.1 " + stringCode.str() + " " + responseStatus(request.getCode()) + "\r\n";
-            response += "Content-Length: " + length.str() + "\r\n";
-            response += "Content-Type: " + getContentType(filePath) + "\r\n\r\n";
+            filePath = m_server.getErrorPage_s();
+            if (!isValidPath(filePath))
+            {
+                filePath = getDefaultError("404");
+            }
+            std::ifstream file(filePath.c_str());
+            if (file)
+            {
+                std::stringstream content;
+                content << file.rdbuf();
+                std::string fileContent = content.str();
+                std::stringstream length;
+                length << fileContent.size();
+                std::stringstream stringCode;
+                stringCode << request.getCode();
+                response = "HTTP/1.1 404 " + responseStatus(404) + "\r\n";
+                response += "Content-Length: " + length.str() + "\r\n";
+                response += "Content-Type: " + getContentType(filePath) + "\r\n\r\n";
 
-            response += fileContent;
+                response += fileContent;
+            }
         }
         else
         {
-            response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            std::ifstream file(filePath.c_str());
+            if (file)
+            {
+                std::stringstream content;
+                content << file.rdbuf();
+                std::string fileContent = content.str();
+                std::stringstream length;
+                length << fileContent.size();
+                std::stringstream stringCode;
+                stringCode << request.getCode();
+                response = "HTTP/1.1 " + stringCode.str() + " " + responseStatus(request.getCode()) + "\r\n";
+                response += "Content-Length: " + length.str() + "\r\n";
+                response += "Content-Type: " + getContentType(filePath) + "\r\n\r\n";
+
+                response += fileContent;
+            }
         }
-    }
-    else
-    {
-        response = "HTTP/1.1 501 Not Implemented\r\n\r\n";
     }
 
     return response;
@@ -133,14 +152,6 @@ std::string Response::obtainFilePath(const std::string &request, Location &locat
         fullPath = location.getRoot() + "/html" + path;
     else
         fullPath = location.getRoot() + path;
-    if (!isValidPath(fullPath))
-    {
-        fullPath = m_server.getErrorPage_s();
-        if(!isValidPath(fullPath))
-        {
-            fullPath = getDefaultError("404");
-        }
-    }
 
     return fullPath;
 }
@@ -324,16 +335,16 @@ std::string Response::responseStatus(int code)
     };
 }
 
-
-
-std::string Response::getDefaultError(std::string code) 
+std::string Response::getDefaultError(std::string code)
 {
 
-    //printDefaultErrors();
+    // printDefaultErrors();
     std::map<std::string, std::string>::iterator it;
 
-    for (it = this->_default_error.begin(); it != this->_default_error.end(); it++) {
-        if (it->first == code) {
+    for (it = this->_default_error.begin(); it != this->_default_error.end(); it++)
+    {
+        if (it->first == code)
+        {
             return it->second;
         }
     }
@@ -341,11 +352,11 @@ std::string Response::getDefaultError(std::string code)
     return "";
 }
 
-
-void Response::printDefaultErrors() 
+void Response::printDefaultErrors()
 {
     std::cout << "Conteúdo de _default_error:" << std::endl;
-    for (std::map<std::string, std::string>::const_iterator it = _default_error.begin(); it != _default_error.end(); ++it) {
+    for (std::map<std::string, std::string>::const_iterator it = _default_error.begin(); it != _default_error.end(); ++it)
+    {
         std::cout << "Código: " << it->first << " - Caminho: " << it->second << std::endl;
     }
 }
